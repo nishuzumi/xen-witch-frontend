6ac8076db70724195e7d6d3fdab87f1600e0b650
Miho <13927203+nishuzumi@users.noreply.github.com>
2022-10-10 13:28:25 +0800

    Draft/recursing tree (#10)
    * 修复数据提示
    
    * 移除log
    
    * 修复白屏问题
    
    * 添加mint的错误提示
    
    * 自动消失

--- src/helper.js
+++ src/helper.js
@@ -27,4 +27,8 @@ export const notification = {
   container: "top-right",
   animationIn: ["animate__animated animate__fadeIn"], // `animate.css v4` classes
   animationOut: ["animate__animated animate__fadeOut"], // `animate.css v4` classes
+  dismiss: {
+    duration: 5000,
+    onScreen: true,
+  },
 };
